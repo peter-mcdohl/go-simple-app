46389bb51a34fa86d6b968c06e821cf998fded52
kevin.turnip <kevin.turnip@cicil.co.id>
2018-10-27 19:59:35 +0700

    add struct

--- main.go
+++ main.go
@@ -6,6 +6,20 @@ import (
 	"net/http"
 )
 
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, r.URL.Path)
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
