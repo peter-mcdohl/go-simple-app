1539b55ced2f01bccbfca76369914621b3507626
kevin.turnip <kevin.turnip@cicil.co.id>
2018-10-29 17:36:19 +0700

    add function get person by id

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
@@ -20,12 +22,40 @@ type Address struct {
 
 var people []Person
 
+// Display all from the people var
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+	for _, item := range people {
+		// if item.ID == params["id"] {
+		json.NewEncoder(w).Encode(item)
+		// return
+		// }
+	}
+	json.NewEncoder(w).Encode(&Person{})
+}
+
+// Display a single data
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, r.URL.Path)
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter()
+	people = append(people, Person{ID: "1", Firstname: "paul", Lastname: "Logan", Address: &Address{City: "Bogor", State: "Indonesia"}})
+	people = append(people, Person{ID: "2", Firstname: "kang", Lastname: "didi", Address: &Address{City: "Bandung", State: "cibereum"}})
+	router.HandleFunc("/", handler)
+	router.HandleFunc("/people", GetPeople).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
